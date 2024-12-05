31e27e633acd4928681c3b51b2ae374a96d7d4c8
Michael Moane <michael.moane@motechsolutions.co.uk>
2024-12-05 22:48:55 +0000

    Update credentials.h

--- credentials.h
+++ credentials.h
@@ -1,39 +1,42 @@
 /*
  * Settings for the Calendar Display
+ *
+ * Timezone: 
+ * https://www.gnu.org/software/libc/manual/html_node/TZ-Variable.html
  */
 
 /* WiFi credentials */
-// Wi-Fi Credentials  
-const char* ssid = "";                                      // WiFi SSID  
-const char* password = "";                   // WiFi password
+const char* ssid            = "";                                      // WiFi SSID
+const char* password        = "";                         // WiFi password
 
-// Calendar Server  
-const char* calkey = "";                                 // Key to access calendar data  
-const char calserver[] = "";                       // Locally hosted Calendar Server URL
+/* Calendar */
+String calkey               = "";                                   // Key to access calendar data
+const char calserver[]      = "SERVER-URL:3000";                             // Calendar Server URL
 
-// OpenWeatherMap API  
-const char* apikey = "";              // API key  
-const char server[] = "api.openweathermap.org";             // URL (don't change)
+/* Open Weathermap Account */
+String apikey               = "";                // Get the developer key for 1000 request/day
+const char server[]         = "api.openweathermap.org";                          // URL (don't change)
 
 /* API Parameters Open Weathermap */
-const char* LAT             = "52.5";                                  // Home location Latitude
-const char* LON             = "-2.08";                                 // Home location Longitude
-const char* City            = "Dudley";                                // Your home city
-const char* Country         = "GB";                                    // Your country code
-const char* Language        = "EN";                                    // Language for weather description
-const char* Hemisphere      = "north"; 
-const char* units           = "metric";                                // Units (imperial or metric)
+String LAT              = "52.5";                         // Home location Latitude
+String LON              = "-2.08";                        // Home location Longitude
+String City             = "Dudley";                       // Your home city See: http://bulk.openweathermap.org/sample/
+String Country          = "GB";                           // Your _ISO-3166-1_two-letter_country_code country code, on OWM find your nearest city and the country code is displayed
+String Language         = "EN";                           // NOTE: Only the weather description is translated by OWM
+String Hemisphere       = "north"; 
+String units            = "metric";  
 
 /* Time and NTP Server */
-const char* ntpServer = ".uk.pool.ntp.org";                         // NTP Server URL  
-const char* timezoneStr = "GMTBST,M3.5./1,M10.5.";                // Timezone Definition
+const char* ntpServer       = "0.uk.pool.ntp.org";                          // URL address
+const char* timezoneStr     = "GMT0BST,M3.5.0/1,M10.5.0";              // Timezone Definition (https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv)
+int gmtOffset_sec           = 0;                                  // Timezone offset of your location in seconds -18000;
 
 /* Sleep Time in Minutes */
 const long SleepDuration    = 30;                                      // Display update interval in minutes
-const int PowerSaveStart    = 22;                                      // Start power save mode
-const int PowerSaveEnd      = 5;                                       // End power save mode
+const int PowerSaveStart    = 22;                                      // Start power save where display is not updated (24 hour format)
+const int PowerSaveEnd      =  5;                                      // End power save where display is not updated (24 hour format)
 
 /* Notification sounds */
 const bool playAlarm        = false;                                   // Play Calendar Alarms
-const bool playBday         = false;                                   // Play Birthday Notifications
-const int playBdayTime      = 10;                                      // Time for birthday notifications
+const bool playBday         = false;                                   // Play Birthday Notifications (true/false)
+const int playBdayTime      =  10;                                     // Play Birthday Notification at full hour (24 hour format)
